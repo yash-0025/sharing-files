4dd79cbe095538b44c7beb5e9cd45f773e19209f
dinesh11515 <dineshaitham2@gmail.com>
2022-06-20 15:10:23 +0530

    added

--- smart-contract/hardhat.config.js
+++ smart-contract/hardhat.config.js
@@ -1,15 +1,17 @@
-require('@nomiclabs/hardhat-waffle')
-require('dotenv').config({ path: '.env' })
-
-const ALCHEMY_API_URL = process.env.ALCHEMY_API_URL
-const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY
-
+/**
+* @type import('hardhat/config').HardhatUserConfig
+*/
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+const { ALCHEMY_API_URL, RINKEBY_PRIVATE_KEY } = process.env;
 module.exports = {
-  solidity: '0.8.4',
-  networks: {
-    rinkeby: {
-      url: ALCHEMY_API_URL,
-      accounts: [RINKEBY_PRIVATE_KEY],
-    },
-  },
+   solidity: "0.8.1",
+   defaultNetwork: "rinkeby",
+   networks: {
+      hardhat: {},
+      rinkeby: {
+         url: ALCHEMY_API_URL,
+         accounts: [RINKEBY_PRIVATE_KEY]
+      }
+   },
 }
--- smart-contract/scripts/deploy.js
+++ smart-contract/scripts/deploy.js
@@ -1,15 +1,16 @@
 const { ethers } = require('hardhat')
 
 const main = async () => {
-  const tinderFactory = await ethers.getContractFactory('TinderERC721')
-  const TinderContract = await tinderFactory.deploy()
+    const nomadsFactory = await ethers.getContractFactory('NomadsERC721')
+    const NomadsContract = await nomadsFactory.deploy()
 
-  console.log('TINDER CONTRACT ADDRESS:', TinderContract.address)
+    console.log('NOMADS CONTRACT ADDRESS:', NomadsContract.address)
 }
 
 main()
-  .then(() => process.exit(0))
-  .catch(error => {
-    console.log('Error in deploying Contract >> ', error)
-    process.exit(1)
-  })
+    .then(() => process.exit(0)
+    .catch(error => {
+        console.log('Error in deploting Contract >> ', error)
+        process.exit(1)
+    })
+    )
